8a6bf97545df73fdf0e382c5cbdc17211762ee0b
niondevi <82332290+niondevi@users.noreply.github.com>
2023-07-13 00:37:46 -0600

    new.htm fix test 22

--- background.js
+++ background.js
@@ -75,7 +75,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 
         // Open the URL in a new tab
         chrome.tabs.create({ url: obsidianUrl , active: true},(newTab) => {
-          setTimeout(() => { chrome.tabs.remove(newTab.id) }, 500);
+          // setTimeout(() => { chrome.tabs.remove(newTab.id) }, 500);
         });
       }
     );
